9372029888a2809dd9366ed3cf52a6652f0732cf
Chohye Han <choiscafe@gmail.com>
2022-07-30 14:49:45 -0400

    button type is added

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Welcome to choiscafe
 
-This webpage is the first webpage created by Chohye Han, aka. Cho!
+This webpage is the first webpage created by Chohye Han.
+The purpose of this project is to practice the pre-work learned regarding HTML, CSS, and Javascript.
--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Get to know Cho!";
 document.querySelector("body").appendChild(h2);
-const h3 = document.createElement("h3");
-h3.textContent= "And this is choiscafe's comment."
-document.querySelector("body").appendChild(h3);
 
 
+function show() {
+  let image = document.getElementById('Aegi');
+  image.src = "photo/Aegi.jpeg"
+
+  document.getElementById('btnID')
+    .style.display = "none";
+}
